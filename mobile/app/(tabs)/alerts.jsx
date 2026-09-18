55ad78af03c0b82569d4891cc9fd1f82e6065b7d
acharyanisha1234 <acharyanisha2063@gmail.com>
2026-09-18 18:16:16 +0545

    feat: add alerts screen placeholder

---
+++ mobile/app/(tabs)/alerts.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function AlertsScreen() {
+  return (
+    <View className="flex-1 bg-dark justify-center items-center">
+      <Text className="text-white text-xl"> Alerts coming soon</Text>
+    </View>
+  );
+}
