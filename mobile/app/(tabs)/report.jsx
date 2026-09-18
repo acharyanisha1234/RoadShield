5fc53f3ec4223e4c507de295827cbced751ec760
acharyanisha1234 <acharyanisha2063@gmail.com>
2026-09-18 18:15:27 +0545

    feat: add report screen

---
+++ mobile/app/(tabs)/report.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function ReportScreen() {
+  return (
+    <View className="flex-1 bg-dark justify-center items-center">
+      <Text className="text-white text-xl"> Report coming soon</Text>
+    </View>
+  );
+}
