416d30159673db2e3819405625c30895ebb411ad
acharyanisha1234 <acharyanisha2063@gmail.com>
2026-09-19 17:12:00 +0545

    feat: add centralized icon mappings

---
+++ mobile/src/theme/icons.js
@@ -0,0 +1,39 @@
+/**
+ * Centralized icon mappings using @expo/vector-icons
+ * Ionicons: outline/filled variants
+ * MaterialCommunityIcons: detailed icons
+ */
+
+export const Ionicons = {
+  shield: 'shield-checkmark',
+  shieldOutline: 'shield-checkmark-outline',
+  location: 'location',
+  locationOutline: 'location-outline',
+  refresh: 'refresh',
+  navigate: 'navigate-circle',
+  navigateOutline: 'navigate-circle-outline',
+  alert: 'warning',
+  alertOutline: 'warning-outline',
+  sos: 'alert-circle',
+  chevronRight: 'chevron-forward',
+  arrowBack: 'arrow-back',
+  search: 'search',
+  camera: 'camera',
+  image: 'images',
+  close: 'close-circle',
+  checkmark: 'checkmark-circle',
+  time: 'time-outline',
+  person: 'person',
+  personOutline: 'person-outline',
+  car: 'car',
+  carSport: 'car-sport',
+  water: 'water',
+  construct: 'construct',
+  ellipse: 'ellipse',
+};
+
+export const MaterialIcons = {
+  road: 'road-variant',
+  traffic: 'traffic-cone',
+  flood: 'home-flood',
+};
