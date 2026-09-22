97c5720e7dfe0dc3990a47483ce9cab644eae6f1
acharyanisha1234 <acharyanisha2063@gmail.com>
2026-09-22 08:28:22 +0545

    feat: add typography design tokens

---
+++ mobile/src/theme/typography.js
@@ -0,0 +1,24 @@
+export const typography = {
+  sizes: {
+    '2xs': 10,
+    xs: 12,
+    sm: 14,
+    base: 16,
+    lg: 18,
+    xl: 20,
+    '2xl': 24,
+    '3xl': 30,
+    '4xl': 36,
+  },
+  weights: {
+    regular: '400',
+    medium: '500',
+    semibold: '600',
+    bold: '700',
+    black: '900',
+  },
+};
+
+export const fontFamilies = {
+  mono: 'monospace',
+};
